898f82871dc7c28277b25302ca3c2fe6f26b4f2e
Fábio Furtili <80091714+fabiofurtili@users.noreply.github.com>
2022-07-26 16:39:15 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,28 @@
+print("Calculadora - Python3")
+#Operadores
+def somar(a,b):
+    return(a+b)
+def subtrair(a,b):
+    return(a-b)
+def multiplicar(a,b):
+    return(a*b)
+def dividir(a,b):
+    return(a/b)
+
+#Entrada de dados
+primeiroValor = int(input("Primeiro valor: "))
+operador = input("Escolha o operador: ")
+segundoValor = int(input("Segundo valor: "))
+
+#Saida de dados
+print(f"{primeiroValor} {operador} {segundoValor} =")
+if operador == "+":
+    print(somar(primeiroValor, segundoValor))
+elif operador == "-":
+    print(subtrair(primeiroValor, segundoValor))
+elif operador == "*":
+    print(multiplicar(primeiroValor, segundoValor))
+elif operador == "/":
+    print(dividir(primeiroValor, segundoValor))
+else:
+    print("Informações incorreta!")
